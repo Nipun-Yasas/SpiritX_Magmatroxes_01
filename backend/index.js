79b7cd9753ecc5aff7971f637e3eb73564547406
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-03-08 15:19:11 +0530

    updated backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express()
+app.use(cors());
 const port = 3000
 
 app.get('/', (req, res) => {
